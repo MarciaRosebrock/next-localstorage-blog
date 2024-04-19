72d62128bc4feeb29243360f8a1ef1af175e761a
Marcia Rosebrock <marcia.rosebrock@gmail.com>
2024-04-19 12:32:07 -0700

    feat: create-blog page

---
+++ next-localstorage-blog/app/create-blog/page.tsx
@@ -0,0 +1,18 @@
+export default function CreateBlogPage() {
+  return (
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <a href="/">Home</a>
+          </li>
+          <li>
+            <a href="/create-blog">Create</a>
+          </li>
+        </ul>
+      </nav>
+      <h1>Create Blog Post</h1>
+      <p>Time to get creative!</p>
+    </div>
+  );
+}
